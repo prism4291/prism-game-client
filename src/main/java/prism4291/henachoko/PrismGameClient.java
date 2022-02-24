5bffea4fd56f804cff303646fb377cf51fe396d1
prism4291 <densikousaku7@gmail.com>
2022-02-24 23:28:01 +0900

    com

---
+++ src/main/java/prism4291/henachoko/PrismGameClient.java
@@ -0,0 +1,25 @@
+package prism4291.henachoko;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+import java.net.URISyntaxException;
+
+public class PrismGameClient {
+    public static void main(String[] args) throws URISyntaxException {
+
+        final Socket socket = IO.socket("https://prism-game-server.herokuapp.com/");
+
+        // サーバーからのmessage_from_serverがemitされた時
+        socket.on("receiveMessage", objects -> {
+            // 最初の引数を表示
+            System.out.println(objects[0]);
+            //サーバー側にmessage_from_clientで送信
+            socket.emit("sendMessage", "This is Java");
+
+        });
+
+        socket.connect();
+    }
+}
